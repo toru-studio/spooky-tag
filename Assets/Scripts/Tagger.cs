770b4f0f79e4c6eabea4a829819ec479eb3846a4
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2023-07-17 22:21:18 +1200

    Added methods for each movement option

--- Assets/Scripts/Tagger.cs
+++ Assets/Scripts/Tagger.cs
@@ -10,7 +10,7 @@ public class Tagger : MonoBehaviour
 
     protected Animator _animator;
     // Start is called before the first frame update
-    void Start()
+    protected void Start()
     {
         _animator = GetComponent<Animator>();
         _characterController = GetComponent<CharacterController>();
@@ -19,6 +19,21 @@ public class Tagger : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void beginClimb()
+    {
+        
+    }
+
+    void beginVault()
+    {
+        
+    }
+
+    void beginSlide()
     {
         
     }
