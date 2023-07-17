ec67e1cd87f5cd9f1bf0d9d48a9c9eae4e404fb4
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2023-07-17 23:04:14 +1200

    Refactored methods and variables
    - Variable names follow c# style (no _)
    - Methods for movement actions are made public

--- Assets/Scripts/Tagger.cs
+++ Assets/Scripts/Tagger.cs
@@ -7,37 +7,37 @@ public class Tagger : MonoBehaviour
 
 
     public Transform orientation;
-    protected Rigidbody _rigidbody;
-    protected Animator _animator;
+    protected Rigidbody rigidbody;
+    protected Animator animator;
 
 
     // Start is called before the first frame update
     protected void Start()
     {
-        _animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
         moveDirection = Vector3.zero;
-        _rigidbody = GetComponent<Rigidbody>();
-        _rigidbody.freezeRotation = true;
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.freezeRotation = true;
     }
 
     //Moves rigidbody by adding force in the direction of moveDirection
     protected void Move(float inputV, float inputH)
     {
         moveDirection = orientation.forward * inputV + orientation.right * inputH;
-        _rigidbody.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
+        rigidbody.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
     }
 
-    void beginClimb()
+    public void beginClimb()
     {
         
     }
 
-    void beginVault()
+    public void beginVault()
     {
         
     }
 
-    void beginSlide()
+    public void beginSlide()
     {
         
     }
