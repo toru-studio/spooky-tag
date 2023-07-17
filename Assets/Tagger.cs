8849fffca8120004393fd2e6f431ee56696beb7c
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2023-07-17 21:26:35 +1200

    Added components to tagger
    Added Fields:
    Animator
    Character Controller
    Move Direction

--- Assets/Tagger.cs
+++ Assets/Tagger.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Tagger : MonoBehaviour
 {
-    
+    protected CharacterController _characterController;
+
+    protected Vector3 moveDirection;
+
+    protected Animator _animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _animator = GetComponent<Animator>();
+        _characterController = GetComponent<CharacterController>();
+        moveDirection = Vector3.zero;
     }
 
     // Update is called once per frame
