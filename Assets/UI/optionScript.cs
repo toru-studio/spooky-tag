b65c6adb91415321c0c4bc292c3c0bd475714acf
nathanbridgeearney <80077609+nathanbridgeearney@users.noreply.github.com>
2023-07-22 01:01:31 +1200

    Audio Slider Implemented
    Now have an audio slider, right now is just linked to a function within optionScript, we can use this for Master, music sliders etc.

---
+++ Assets/UI/optionScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class optionScript : MonoBehaviour
+{
+
+    public void setVolume(float vol)
+    {
+        print("Volume: "+vol);
+    }
+
+    public void setResolution()
+    {
+        
+    }
+
+}
