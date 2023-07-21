706fac90d9275b5b9844917b7318544c3d0bb97e
nathanbridgeearney <80077609+nathanbridgeearney@users.noreply.github.com>
2023-07-22 03:17:10 +1200

    Keybind changer implemented
    Created script to allow for the changing of keybinds when linked to a button. This can be expanded to however many keybinds we want to give the user.
    
    Co-Authored-By: Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>

--- Assets/UI/optionScript.cs
+++ Assets/UI/optionScript.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class optionScript : MonoBehaviour
 {
+    private keyBinds currentKeyState;
+    private KeyCode lastPressed;
+    private bool waiting = false;
+
+    enum keyBinds
+    {
+        jump,
+        crouch,
+        sprint
+    }
+
+
+    void OnGUI()
+    {
+        if (waiting){
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                changeKeyBind(e.keyCode);
+            }
+        }
+    }
 
     public void setVolume(float vol)
     {
-        print("Volume: "+vol);
+        print("Volume: " + vol);
     }
 
-    public void setResolution()
+    public void changeKeyState(string text)
     {
-        
+        waiting = true;
+        currentKeyState = text switch
+        {
+            "jump" => keyBinds.jump,
+            "sprint" => keyBinds.sprint,
+            "crouch" => keyBinds.crouch,
+            _ => currentKeyState
+        };
     }
 
+    private void changeKeyBind(KeyCode key)
+    {
+        switch (currentKeyState)
+        {
+            case keyBinds.jump:
+                PlayerMovement.jumpKey = key;
+                break;
+            case keyBinds.sprint:
+                PlayerMovement.sprintKey = key;
+                break;
+            case keyBinds.crouch:
+                PlayerMovement.crouchKey = key;
+                break;
+        }
+        waiting = false;
+    }
 }
