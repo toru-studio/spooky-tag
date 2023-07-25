db421a7cfd19ae7f87e8b1217a02e85b2a4fa100
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2023-07-25 21:18:37 +1200

    Fixed build issues

---
+++ Assets/SatorImaging/PoseEditor/Runtime/PoseEditorManager.cs
@@ -0,0 +1,76 @@
+﻿#if UNITY_EDITOR
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System;
+
+
+
+namespace SatorImaging.PoseEditor
+{
+    public class PoseEditorManager : MonoBehaviour
+    {
+        static void AddPoseEditor()
+        {
+
+        }
+
+
+
+
+
+        public void RemovePoseEditor()
+        {
+        }
+
+
+
+        
+        
+        public void UpdatePoseEditor()
+        {
+        }
+
+
+        // update finger bone children here.
+        void OnValidate()
+        {
+
+        }
+
+
+
+
+
+
+
+        // draw gizmos while inactive
+        void OnDrawGizmos()
+        {
+        }
+
+
+
+
+        public void DrawHandle(bool colorize = true)
+        {
+        }
+
+
+
+
+        public void OnSelectionChanged()
+        {
+        }
+
+
+
+
+
+
+
+    }//class
+}//namespace
+#endif
